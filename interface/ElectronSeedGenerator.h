8f0a62d0de47ceaf431a57dd6bda7e2ba6b91c90
David Chamont <sha1-cc2cbd4cf572176bc632dcdb55d943ce51d4e6af@cern.ch>
2010-05-12 16:20:22 +0000

    add a configurable InputTag for the beamspot

--- interface/ElectronSeedGenerator.h
+++ interface/ElectronSeedGenerator.h
@@ -65,6 +65,10 @@ class ElectronSeedGenerator
   bool fromTrackerSeeds_;
   //  edm::InputTag initialSeeds_;
 
+  double zmin1_, zmax1_;
+  edm::Handle<reco::BeamSpot> theBeamSpot;
+  edm::InputTag beamSpotTag_;
+
   float lowPtThreshold_;
   float highPtThreshold_;
   float nSigmasDeltaZ1_;
@@ -73,8 +77,6 @@ class ElectronSeedGenerator
   float deltaPhi1Low_, deltaPhi1High_;
   float deltaPhi2_;
 
-  double zmin1_, zmax1_;
-  edm::Handle<reco::BeamSpot> theBeamSpot;
 
   PixelHitMatcher *myMatchEle;
   PixelHitMatcher *myMatchPos;
